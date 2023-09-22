b1fe63fe5867d0994df699a0908f8f3e3f3a4748
ridaarif98 <ridaarif16@gmail.com>
2023-09-22 17:35:26 +0200

    Fix form and fields

--- src/components/RegisterForm/FormTwo.js
+++ src/components/RegisterForm/FormTwo.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
-import Webcam from 'react-webcam';
-import { useNavigate } from 'react-router';
-import image1 from '../images/image1.jpg';
+import { useNavigate } from 'react-router-dom';
+import FaceDetection from '../facedetection/FaceDetection';
 import btnImg from '../images/WITH-ALPHA-CHANNEL_GIhan_BTV_Creation_2nd-option__the-better-Y-BUTTON_Fiverr-Test_.gif';
 
 const FormTwo = () => {
@@ -9,25 +8,15 @@ const FormTwo = () => {
   const [currentStep, setCurrentStep] = useState(1);
   const [error, setError] = useState(null);
   const navigate = useNavigate();
+
   const totalSections = 3;
   const [formData, setFormData] = useState({
-    user_image: '',
+    productId: '2',
+    image: '',
     firstName: '',
     lastName: '',
     email: '',
   });
-  const [showCamera, setShowCamera] = useState(false);
-
-  const webcamRef = React.useRef(null);
-
-  const captureImage = () => {
-    const imageSrc = webcamRef.current.getScreenshot();
-    setCapturedImage(imageSrc);
-  };
-
-  //   const clearImage = () => {
-  //     setCapturedImage(null);
-  //   };
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -39,13 +28,13 @@ const FormTwo = () => {
 
   const handleNext = () => {
     // Move to the next section
+    console.log(capturedImage);
     setCurrentStep((prevSection) => (prevSection < totalSections ? prevSection + 1 : prevSection));
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    // const formImage = new FormData();
-    // formImage.append('user_image', captureImage)
+    console.log(formData);
     try {
       const response = await fetch('https://api.yaavaay.com/v1/users', {
         method: 'POST',
@@ -56,8 +45,8 @@ const FormTwo = () => {
       });
       if (response.ok) {
         // Handle successful API response
-        // const userData = await response.json();
-        // const newUserId = userData.user.id;
+        const userData = await response.json();
+        console.log(userData);
         navigate('./payment');
       } else if (response.status === 400) {
         throw new Error('Bad request: the data provided is invalid');
@@ -80,82 +69,58 @@ const FormTwo = () => {
         return (
           <div className="input-main">
             <div className="input-container">
+
               {capturedImage ? (
                 <div className="captured-image-container">
+                  {' '}
                   <img src={capturedImage} alt="Captured" />
                 </div>
-              ) : (
-                <div className="webcam-container">
-                  {showCamera ? (
-                    <Webcam
-                      className="webcam"
-                      audio={false}
-                      ref={webcamRef}
-                      screenshotFormat="image/jpeg"
-                    />
-                  ) : (
-                /* eslint-disable*/ 
-                <img
-                  src={image1}
-                  className="webcam-img"
-                  alt="Captured"
-                  onClick={() => setShowCamera(true)} // Clicking the image turns on the camera.
-                  style={{ cursor: 'pointer' }}
-                />
-              )}
-
-              {showCamera ? (
-                <button type="button" className="capture" onClick={captureImage}>
-                  Y
-                </button>
-              ) : null}
+              ) : (<FaceDetection setCapturedImage={setCapturedImage} setFormData={setFormData} />)}
             </div>
-          )}
-        </div>
-      </div>
+          </div>
         );
       case 2:
         return (
           <div className="input-main">
-                   <div className="input-container">
-                     <div className="any">
-                       <input
-                         type="text"
-                         name="firstName"
-                         placeholder="First Name"
-                         onChange={handleChange}
-                         required
-                         minLength={3}
-                         maxLength={13}
-                       />
-                       <input
-                         type="text"
-                         name="lastName"
-                         placeholder="Last Name"
-                         onChange={handleChange}
-                         required
-                         minLength={3}
-                         maxLength={13}
-                       />
-                     </div>
-                   </div>
- 
-                 </div>
+            <div className="input-container">
+              <div className="any">
+                <input
+                  type="text"
+                  name="firstName"
+                  placeholder="First Name"
+                  onChange={handleChange}
+                  required
+                  minLength={3}
+                  maxLength={13}
+                />
+                <input
+                  type="text"
+                  name="lastName"
+                  placeholder="Last Name"
+                  onChange={handleChange}
+                  required
+                  minLength={3}
+                  maxLength={13}
+                />
+              </div>
+            </div>
+
+          </div>
         );
       case 3:
         return (
           <div className="input-main">
-          <div className="input-container">
-            <input
-              type="email"
-              name="email"
-              placeholder="Email"
-              required
-              onChange={handleChange}
-            />
-            {/* <button type="button" className="capture">y</button> */}
+            <div className="input-container">
+              <input
+                type="email"
+                name="email"
+                placeholder="Email"
+                onChange={handleChange}
+                required
+              />
+              {/* <button type="button" className="capture">y</button> */}
+            </div>
           </div>
-        </div>
         );
       default:
         return null;
@@ -163,17 +128,19 @@ const FormTwo = () => {
   };
 
   return (
-   
+
     <div className="diff-container">
-        <form onSubmit={handleSubmit}>
-            {renderSection()}
-              {/* <button onClick={handlePrevious} disabled={currentStep === 1}>Previous</button> */}
-              {currentStep < totalSections && (
-              <button onClick={handleNext} type="button" className='button-names'> <img src={btnImg} alt="y-logo" /></button>
-            )} 
-              {currentStep=== totalSections && (<button type='submit' className='button-names'><img src={btnImg} alt="y-logo" /></button>
-            )}
-        </form>
+      <form onSubmit={handleSubmit}>
+        {renderSection()}
+        {currentStep < totalSections && (
+          <button onClick={handleNext} type="button" className="button-names">
+            {' '}
+            <img src={btnImg} alt="y-logo" />
+          </button>
+        )}
+        {currentStep === totalSections && (<button type="submit" className="button-names"><img src={btnImg} alt="y-logo" /></button>
+        )}
+      </form>
       {error && <p>{error}</p>}
     </div>
   );
